9b24752f61d853b84b45bca967ea6cdd0f4abb0a
unusali1 <yali71507@gmail.com>
2022-09-28 01:33:28 +0600

    Router-dom-manage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Products from "./Products"
+import Contact from "./Contact"
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart"
+import ErrorPage from "./ErrorPage";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ < Home />} />
+        <Route path="/about" element={ < About />} />
+        <Route path="/products" element={ < Products />} />
+        <Route path="/contact" element={ < Contact />} />
+        <Route path="/singleproduct/:id" element={ < SingleProduct />} />
+        <Route path="/cart" element={ < Cart />} />
+        <Route path="*" element={ < ErrorPage /> } />
+        
+      </Routes>
+    
+    </BrowserRouter>
+  )
 };
 
 export default App;
