f787791f91dd5f31e6073f8fd318d8c05c70738c
unusali1 <yali71507@gmail.com>
2022-09-28 03:49:52 +0600

    Header Styled

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Cart from "./Cart"
 import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 
 const App = () => {
  
@@ -36,6 +37,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
     <BrowserRouter>
      <GlobalStyle />
+     <Header />
       <Routes>
         <Route path="/" element={ < Home />} />
         <Route path="/about" element={ < About />} />
